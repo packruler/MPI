46c29daa61851aa103e7bc819da3b9b44677579f
packruler <packruler@gmail.com>
2016-02-18 17:57:15 -0600

    modified:   benchmark 	modified:   benchmark.cpp 	modified:   run.sh

--- benchmark.cpp
+++ benchmark.cpp
@@ -5,10 +5,10 @@
 #include <time.h>
 
 // Creates an array of random numbers. Each number has a value from 0 - 1
-float *create_rand_nums(int num_elements) {
+float *create_rand_nums(long num_elements) {
   float *rand_nums = (float *)malloc(sizeof(float) * num_elements);
   assert(rand_nums != NULL);
-  int i;
+  long i;
   for (i = 0; i < num_elements; i++) {
     rand_nums[i] = (rand() / (float)RAND_MAX);
   }
@@ -16,9 +16,9 @@ float *create_rand_nums(int num_elements) {
 }
 
 // Computes the average of an array of numbers
-float compute_avg(float *array, int num_elements) {
+float compute_avg(float *array, long num_elements) {
   float sum = 0.f;
-  int i;
+  long i;
   for (i = 0; i < num_elements; i++) {
     sum += array[i];
   }
@@ -26,28 +26,77 @@ float compute_avg(float *array, int num_elements) {
 }
 
 int main(int argc, char **argv) {
-  int rank, size;
-
-  MPI_Init(&argc, &argv);               /* starts MPI */
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank); /* get current process id */
-  MPI_Comm_size(MPI_COMM_WORLD, &size); /* get number of processes */
-  double sum = 0;
-  int array[1000000000];
-  srand(time(NULL));
-  long start = time(NULL);
-  for (int i; i < sizeof(array); i++) {
-    array[i] = rand();
+  if (argc != 2) {
+    fprintf(stderr, "Usage: avg num_elements_per_proc\n");
+    exit(1);
   }
 
-  // int val;
-  // for (long i = 0; i < 1000000000; i++) {
-  //   if (i % size == rank) {
-  //     sum += i;
-  //   }
-  // }
-  start = time(NULL) - start;
-  printf("Sum: %f\n", sum);
-  printf("Process %d took %ld seconds\n", rank, start);
+  long num_elements_per_proc = atol(argv[1]);
+  printf("%ld\n", num_elements_per_proc);
+  // Seed the random number generator to get different results each time
+  srand(time(NULL));
+
+  MPI_Init(NULL, NULL);
+
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  // Create a random array of elements on the root process. Its total
+  // size will be the number of elements per process times the number
+  // of processes
+  float *rand_nums = NULL;
+  if (world_rank == 0) {
+    rand_nums = create_rand_nums(num_elements_per_proc * world_size);
+  }
+
+  // For each process, create a buffer that will hold a subset of the entire
+  // array
+  float *sub_rand_nums = (float *)malloc(sizeof(float) * num_elements_per_proc);
+  assert(sub_rand_nums != NULL);
+
+  long start = time(NULL);
+  // Scatter the random numbers from the root process to all processes in
+  // the MPI world
+  MPI_Scatter(rand_nums, num_elements_per_proc, MPI_FLOAT, sub_rand_nums,
+              num_elements_per_proc, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+  // Compute the average of your subset
+  float sub_avg = compute_avg(sub_rand_nums, num_elements_per_proc);
+
+  // Gather all partial averages down to the root process
+  float *sub_avgs = NULL;
+  if (world_rank == 0) {
+    sub_avgs = (float *)malloc(sizeof(float) * world_size);
+    assert(sub_avgs != NULL);
+  }
+  MPI_Gather(&sub_avg, 1, MPI_FLOAT, sub_avgs, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+  // Now that we have all of the partial averages on the root, compute the
+  // total average of all numbers. Since we are assuming each process computed
+  // an average across an equal amount of elements, this computation will
+  // produce the correct answer.
+  if (world_rank == 0) {
+    float avg = compute_avg(sub_avgs, world_size);
+    printf("Avg of all elements is %f\n", avg);
+    // Compute the average across the original data for comparison
+    float original_data_avg =
+        compute_avg(rand_nums, num_elements_per_proc * world_size);
+    printf("Avg computed across original data is %f\n", original_data_avg);
+
+    printf("The job took %ld seconds with %d processes\n", (time(NULL) - start),
+           world_size);
+  }
+
+  // Clean up
+  if (world_rank == 0) {
+    free(rand_nums);
+    free(sub_avgs);
+  }
+  free(sub_rand_nums);
+
+  MPI_Barrier(MPI_COMM_WORLD);
   MPI_Finalize();
   return 0;
 }
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
 mpicc benchmark.cpp -o benchmark
-mpiexec -np 4 ./benchmark
+mpiexec -np 4 ./benchmark 100000000
