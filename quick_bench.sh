78a618e37cec5c07a6128c82850e554ff48c6259
packruler <packruler@gmail.com>
2016-02-21 16:27:46 -0500

    modified:   benchmark 	modified:   quick_bench.sh

--- quick_bench.sh
+++ quick_bench.sh
@@ -6,14 +6,22 @@ MAX_NUM_PROCESSES=36
 OUTPUT_FILE="benchmark.txt"
 
 echo > $OUTPUT_FILE
-for i in $(seq 1 $MAX_NUM_PROCESSES); do
-   clear
-   echo "Working with $i processes"
+PROCESS_COUNT=1
+
+while [[ $PROCESS_COUNT -le $MAX_NUM_PROCESSES ]]
+ do
+   # clear
+   echo "Working with $PROCESS_COUNT processes"
    START=`date +%s%N | cut -b1-13`
-   mpiexec -np $i ./benchmark 10000000
+   mpiexec -np $PROCESS_COUNT ./benchmark 10000000
 
    END=`date +%s%N | cut -b1-13`
 
    DURATION=`expr $END - $START`
-   echo "$i processes took $DURATION milliseconds to complete" >> $OUTPUT_FILE
+   echo "$PROCESS_COUNT processes took $DURATION milliseconds to complete" >> $OUTPUT_FILE
+   if [[ $PROCESS_COUNT -eq 32 ]]; then
+      PROCESS_COUNT=36
+   else
+      ((PROCESS_COUNT = PROCESS_COUNT * 2))
+   fi
 done
