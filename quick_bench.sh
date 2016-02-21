240e077c669c12b3ac4cb6940e3370729beabffb
packruler <packruler@gmail.com>
2016-02-21 15:54:17 -0500

    new file:   quick_bench.sh

---
+++ quick_bench.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mpicc benchmark.cpp -o benchmark
+
+MAX_NUM_PROCESSES=36
+
+OUTPUT_FILE="benchmark.txt"
+
+echo > $OUTPUT_FILE
+for i in $(seq 1 $MAX_NUM_PROCESSES); do
+   clear
+   echo "Working with $i processes"
+   START=`date +%s%N | cut -b1-13`
+   mpiexec -np $i ./benchmark 1000000
+
+   END=`date +%s%N | cut -b1-13`
+
+   DURATION=`expr $END - $START`
+   echo "$i processes took $DURATION milliseconds to complete" >> $OUTPUT_FILE
+done
